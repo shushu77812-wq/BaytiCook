269a8b41a5749f67c311f086f0fd086723a596b3
shushu77812-wq <shushu77812@gmail.com>
2026-04-20 22:56:43 +0300

    fix typo

--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,7 @@ def create_app():
 
     app.config["SECRET_KEY"] = "123456789"
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")  
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     ##app.config["SESSION_PERMANENT"] = False   # 🔥 مهم للسلة
