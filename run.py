c33a6ada332ac3f6dc9f15e3c8d0fc4093f3316d
shushu77812-wq <shushu77812@gmail.com>
2026-04-22 00:47:36 +0300

    fix admem

--- run.py
+++ run.py
@@ -1,11 +1,18 @@
 from app import create_app, db
 import os
+from werkzeug.security import generate_password_hash
+from app.models.user_model import User
 
 app = create_app()
 
-# 🔥 هذا أهم سطر (ينشئ الجداول)
 with app.app_context():
     db.create_all()
+
+    # 🔥 تعديل كلمة مرور الادمن
+    user = User.query.filter_by(role="admin").first()
+    if user:
+        user.password_hash = generate_password_hash("123456")
+        db.session.commit()
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
