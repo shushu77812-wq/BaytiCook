e056159c983de1c147c18509a55c2805af325f9d
shushu77812-wq <shushu77812@gmail.com>
2026-04-24 00:58:50 +0300

    fix basee

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail
 import os
 
 db = SQLAlchemy()
+mail = Mail()   # ✅ تهيئة البريد
 
 def create_app():
     app = Flask(__name__)
@@ -13,9 +15,17 @@ def create_app():
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-    db.init_app(app)
+    # ✅ إعدادات البريد
+    app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+    app.config['MAIL_PORT'] = 587
+    app.config['MAIL_USE_TLS'] = True
+    app.config['MAIL_USERNAME'] = os.environ.get("MAIL_USERNAME")  # بريدك
+    app.config['MAIL_PASSWORD'] = os.environ.get("MAIL_PASSWORD")  # كلمة مرور التطبيقات
 
-    # ✅ استدعاء الموديلات (بدون مشاكل import *)
+    db.init_app(app)
+    mail.init_app(app)   # ✅ ربط البريد مع التطبيق
+
+    # ✅ استدعاء الموديلات
     from app.models.kitchen_model import Kitchen
     from app.models.meal_model import Meal
     from app.models.order_model import Order
