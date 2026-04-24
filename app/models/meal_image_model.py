de251488010ea2548797909a3f13632c4d2e0d3b
shushu77812-wq <shushu77812@gmail.com>
2026-04-25 00:43:59 +0300

    add MealImage model and import11

--- app/models/meal_image_model.py
+++ app/models/meal_image_model.py
@@ -3,12 +3,11 @@ from app import db
 class MealImage(db.Model):
     __tablename__ = "meal_images"
 
-    id = db.Column(db.Integer, primary_key=True)
-    meal_id = db.Column(db.Integer, db.ForeignKey("meal.id"), nullable=False)
-    image_url = db.Column(db.String(255), nullable=False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    image = db.Column(db.String(255), nullable=False)
 
-    # علاقة مع جدول Meal
-    meal = db.relationship("Meal", backref="images")
+    meal_id = db.Column(db.Integer, db.ForeignKey("meals.id"), nullable=False)
+    meal = db.relationship("Meal", back_populates="images")
 
     def __repr__(self):
-        return f"<MealImage {self.id} - {self.image_url}>"
+        return f"<MealImage {self.image}>"
