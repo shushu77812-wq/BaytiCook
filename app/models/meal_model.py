5250d31d8899b856737820ff654a122a2fb97ea6
shushu77812-wq <shushu77812@gmail.com>
2026-04-25 00:51:09 +0300

    fix all mshroa

--- app/models/meal_model.py
+++ app/models/meal_model.py
@@ -26,18 +26,3 @@ class Meal(db.Model):
 
     def __repr__(self):
         return f"<Meal {self.name}>"
-
-# =============================
-# جدول صور الأكلات
-# =============================
-class MealImage(db.Model):
-    __tablename__ = "meal_images"
-
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    image = db.Column(db.String(255), nullable=False)
-
-    meal_id = db.Column(db.Integer, db.ForeignKey("meals.id"), nullable=False)
-    meal = db.relationship("Meal", back_populates="images")
-
-    def __repr__(self):
-        return f"<MealImage {self.image}>"
